ce82e7826fcdeddb690afd3447311bb89a094b20
Gunics Roland <grolcsi09@gmail.com>
2025-01-17 15:44:51 +0100

    File reading added

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 /*** includes ***/
 
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
@@ -169,16 +173,25 @@ int getWindowSize(int *rows, int *cols) {
 
 /*** file i/o ***/
 
-void editorOpen() {
-	char *line = "Hello, world!";
-	ssize_t linelen = 13;
+void editorOpen(char *filename) {
+	FILE *fp = fopen(filename, "r");
+	if (!fp) die("fopen");
 
-	E.row.size = linelen;
-	E.row.chars = malloc(linelen + 1);
-	memcpy(E.row.chars, line, linelen);
-	E.row.chars[linelen] = '\0';
-	E.numrows = 1;	
-
+	char *line = NULL;
+	size_t linecap = 0;
+	ssize_t linelen;
+	linelen = getline(&line, &linecap, fp);
+	if (linelen != -1) {
+		while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+			linelen--;
+		E.row.size = linelen;
+		E.row.chars = malloc(linelen + 1);
+		memcpy(E.row.chars, line, linelen);
+		E.row.chars[linelen] = '\0';
+		E.numrows = 1;
+	}
+	free(line);
+	fclose(fp);
 }
 
 /*** append buffer ***/
@@ -210,7 +223,7 @@ void editorDrawRows(struct abuf *ab) {
 	int padding;
 	for (y = 0; y < E.screenrows; y++) {
 		if (y >= E.numrows) {
-			if (y == E.screenrows / 3) {
+			if (E.numrows == 0 && y == E.screenrows / 3) {
 				char welcome[80];
 				int welcomelen = snprintf(welcome, sizeof(welcome),
 					"Atto editor -- version %s", ATTO_VERSION);
@@ -327,10 +340,12 @@ void initEditor() {
 		die("getWindowSize");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
 	enableRawMode();	
 	initEditor();
-	editorOpen();
+	if (argc >= 2) {
+		editorOpen(argv[1]);
+	}
 
 	while (1){
 		editorRefreshScreen();
