6eee7b7dfbccc1a86c5363aa313e4a86ba669592
Gunics Roland <grolcsi09@gmail.com>
2025-01-17 17:40:46 +0100

    Tab rendering as spaes

--- main.c
+++ main.c
@@ -16,8 +16,11 @@
 
 /*** defines ***/
 
-#define CTRL_KEY(k) ((k) & 0x1f)
 #define ATTO_VERSION "0.0.65"
+#define ATTO_TAB_STOP 8
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 
 enum editorKey {
 	ARROW_LEFT = 1000,
@@ -35,7 +38,9 @@ enum editorKey {
 
 typedef struct erow {
 	int size;
+	int rsize;
 	char *chars;
+	char *render;
 } erow;
 
 struct editorConfig{
@@ -175,6 +180,29 @@ int getWindowSize(int *rows, int *cols) {
 
 /*** row operations ***/
 
+void editorUpdateRow(erow *row) {
+	int tabs = 0;
+	int j;
+	for (j =0; j < row->size; j++)
+		if (row->chars[j] == '\t') tabs++;
+
+	free(row->render);
+	row->render = malloc(row->size + tabs * (ATTO_TAB_STOP - 1) + 1);
+
+	int idx = 0;
+	for (j = 0; j < row->size; j++) {
+		if (row->chars[j] == '\t') {
+			row->render[idx++] = ' ';
+			while (idx % ATTO_TAB_STOP != 0) row->render[idx++] = ' ';
+		}
+		else {
+			row->render[idx++] = row->chars[j];
+		}
+	}
+	row->render[idx] = '\0';
+	row->rsize = idx;
+}
+
 void editorAppendRow(char *s, size_t len) {
 	E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
 
@@ -183,6 +211,11 @@ void editorAppendRow(char *s, size_t len) {
 	E.row[at].chars = malloc(len + 1);
 	memcpy(E.row[at].chars, s, len);
 	E.row[at].chars[len] = '\0';
+
+	E.row[at].rsize = 0;
+	E.row[at].render = NULL;
+	editorUpdateRow(&E.row[at]);
+
 	E.numrows++;
 }
 
@@ -269,10 +302,10 @@ void editorDrawRows(struct abuf *ab) {
 			}
 		}
 		else {
-			int len = E.row[filerow].size - E.coloff;
+			int len = E.row[filerow].rsize - E.coloff;
 			if (len < 0) len = 0;
 			if (len > E.screencols) len = E.screencols;
-			abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+			abAppend(ab, &E.row[filerow].render[E.coloff], len);
 		}
 
 		abAppend(ab, "\x1b[K", 3);
