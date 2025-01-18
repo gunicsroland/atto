ec397b1854c04950aa0b8e4ed20ceb4de4a06c53
Gunics Roland <grolcsi09@gmail.com>
2025-01-18 11:18:31 +0100

    Char deletion

--- main.c
+++ main.c
@@ -256,6 +256,14 @@ void editorRowInsertChar(erow *row, int at, int c) {
 	E.dirty++;
 }
 
+void editorRowDelChar(erow *row, int at) {
+	if (at < 0 || row->size <= at) return;
+	memmove(&row->chars[at], &row->chars[at + 1], row->size -at);
+	row->size--;
+	editorUpdateRow(row);
+	E.dirty++;
+}
+
 /*** editor operations ***/
 
 void editorInsertChar(int c) {
@@ -264,6 +272,16 @@ void editorInsertChar(int c) {
 	}
 	editorRowInsertChar(&E.row[E.cy], E.cx, c);
 	E.cx++;
+}
+
+void editorDelChar() {
+	if (E.cy == E.numrows) return;
+
+	erow *row = &E.row[E.cy];
+	if (E.cx > 0) {
+		editorRowDelChar(row, E.cx - 1);
+		E.cx--;
+	}
 }
 
 /*** file i/o ***/
@@ -542,6 +560,8 @@ void editorProcessKeypress() {
 		case BACKSPACE:
 		case CTRL_KEY('h'):
 		case DEL_KEY:
+			if (c == DEL_KEY) editorMoveCursor(ARROW_RIGHT);
+			editorDelChar();
 			break;
 
 		case PAGE_UP:
