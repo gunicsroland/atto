9a2316cc6049e59f74d4fbbe646a10b28000b406
Gunics Roland <grolcsi09@gmail.com>
2025-01-18 15:03:21 +0100

    Find fuction added

--- main.c
+++ main.c
@@ -19,7 +19,7 @@
 
 /*** defines ***/
 
-#define ATTO_VERSION "0.1.0"
+#define ATTO_VERSION "0.1.9"
 #define ATTO_TAB_STOP 8
 #define ATTO_QUIT_TIMES 2
 
@@ -206,6 +206,19 @@ int editorRowCxToRx(erow *row, int cx) {
 	}
 
 	return rx;
+}
+
+int editorRowRxToCx(erow *row, int rx) {
+	int cur_rx = 0.
+	int cx;
+	for (cx = 0; cx < row->size; cx++) {
+		if (row->chars[cx] == '\t')
+			cur_rx += (ATTO_TAB_STOP - 1) - (cur_rx % ATTO_TAB_STOP);
+		cur_rx++;
+
+		if (cur_rx > rx) return cx;
+	}
+	return cx;
 }
 
 void editorUpdateRow(erow *row) {
@@ -403,6 +416,27 @@ void editorSave() {
 
 	free(buf);
 	editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
+}
+
+/*** find ***/
+
+void editorFind() {
+	char *query = editorPrompt("Search: %s (Esc to cancel)");
+	if (query == NULL) return;
+
+	int i;
+	for (i = 0; i < E.numrows; i++) {
+		erow *row = &E.row[i];
+		char *match = strstr(row->render, query);
+		if (match) {
+			E.cy = i;
+			E.cx = editorTowRxToCx(row, match - row->render);
+			E.rowoff = E.numrows;
+			break;
+		}
+	}
+
+	free(query);
 }
 
 /*** append buffer ***/
@@ -650,6 +684,10 @@ void editorProcessKeypress() {
 		case END_KEY:
 			if (E.cy < E.numrows) E.cx = E.row[E.cy].size;
 			break;
+		case CTRL_KEY('f'):
+			editorFind();
+			break;
+
 		case BACKSPACE:
 		case CTRL_KEY('h'):
 		case DEL_KEY:
@@ -720,7 +758,7 @@ int main(int argc, char *argv[]) {
 		editorOpen(argv[1]);
 	}
 
-	editorSetStatusMessage("^Q - quit  ^S - Save");
+	editorSetStatusMessage("^Q - quit  ^S - Save  ^F - Find");
 
 	while (1){
 		editorRefreshScreen();
